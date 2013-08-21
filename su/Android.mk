b8cd947b1c19cb66fc6fa1da9d0f87c046c488e2
Chih-Wei Huang <cwhuang@linux.org.tw>
2013-08-21 14:03:12 +0800

    su: disable the legacy su binary
    Use the su binary from Superuser if SUPERUSER_EMBEDDED is defined.

--- su/Android.mk
+++ su/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(SUPERUSER_EMBEDDED),true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -13,3 +14,4 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := debug
 
 include $(BUILD_EXECUTABLE)
+endif
